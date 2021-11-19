07a82ffe3ce73850ac67773d74a3eb858f8fae27
Marco <marco.cusenza.me@gmail.com>
2021-11-19 16:59:02 +0100

    array e prototipo funzione createCard

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const team = [
+    {
+        "name": "Wayne Barnett",
+        "role": "Founder & CEO",
+        "img": "img/wayne-barnett-founder-ceo.jpg",
+    },
+
+    {
+        "name": "Angela Caroll",
+        "role": "Chief Editor",
+        "img": "img/angela-caroll-chief-editor.jpg",
+    },
+
+    {
+        "name": "Walter Gordon",
+        "role": "Office Manager",
+        "img": "img/walter-gordon-office-manager.jpg",
+    },
+
+    {
+        "name": "Angela Lopez",
+        "role": "Social Media Manager",
+        "img": "img/angela-lopez-social-media-manager.jpg",
+    },
+
+    {
+        "name": "Scott Estrada",
+        "role": "Developer",
+        "img": "img/scott-estrada-developer.jpg",
+    },
+
+    {
+        "name": "Barbara Ramos",
+        "role": "Graphic Designer",
+        "img": "img/barbara-ramos-graphic-designer.jpg",
+    },
+
+];
+
+function createCard(i) {
+    const person = team[i];
+    document.querySelector("team-container").innerHTML += `
+    <div class="team-card">
+        <div class="card-image">
+            <img src="${person.img}" alt="${person.name}"/>
+        </div>
+        <div class="card-text">
+            <h3>${person.name}</h3>
+            <p>${person.role}</p>
+        </div>
+    </div>
+    `
+}
